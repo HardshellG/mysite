5ced3724d7bcc811af3c317022523353f2257840
Harshal Gulhane <98465047+HardshellG@users.noreply.github.com>
2023-09-29 23:57:16 +0530

    Update jee.py

--- pages/jee.py
+++ pages/jee.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import altair as alt
+
+
 st.title("Jee")
 
 df=pd.read_csv("C:/Users/Harshan/Desktop/mysite/Total Seats.csv")
