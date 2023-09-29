08811392715c60bb518fe96c1864640141e444c9
Harshal Gulhane <98465047+HardshellG@users.noreply.github.com>
2023-09-30 00:01:17 +0530

    Update jee.py

--- pages/jee.py
+++ pages/jee.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 st.title("Jee")
 
-df=pd.read_csv("C:/Users/Harshan/Desktop/mysite/Total Seats.csv")
+df=pd.read_csv("Total Seats.csv")
 st.write(df)
 
 st.bar_chart(df, x="Branch of Study", y="Total Seats" )
-dg=pd.read_csv("C:/Users/Harshan/Desktop/mysite/All IIT List of Percentile.csv")
+dg=pd.read_csv("All IIT List of Percentile.csv")
 st.write(dg)
 st.bar_chart(dg,x="IIT/Branch", y=["General","SC","ST","OBC"] )
 
